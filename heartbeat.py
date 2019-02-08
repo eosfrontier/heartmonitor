97c60d94e49e7ada902e11825a594e38c3b8c5a6
Willem Monsuwe <willem@monsuwe.nl>
2019-02-08 15:56:25 +0000

    NFC lezer er bij

--- heartbeat.py
+++ heartbeat.py
@@ -1,10 +1,58 @@
 #!/usr/bin/env python
 
-import time, sys, os, random, smbus
+import time, sys, os, random, smbus, ctypes
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 import pyaudio, wave
 from neopixel import *
 import MAX30102
+import nfc
+
+class NFCReader(object):
+    MC_AUTH_A = 0x60
+    MC_AUTH_B = 0x61
+    MC_READ = 0x30
+    MC_WRITE = 0xA0
+    card_timeout = 10
+
+    def __init__(self):
+        self.__context = None
+        self.__device = None
+        
+        self._card_present = False
+        self._card_last_seen = None
+        self._card_uid = None
+        
+        mods = [(nfc.NMT_ISO14443A, nfc.NBR_106)]
+        self.__modulations = (nfc.nfc_modulation * len(mods))()
+        for i in range(len(mods)):
+            self.__modulations[i].nmt = mods[i][0]
+            self.__modulations[i].nbr = mods[i][1]
+
+    def run(self):
+        self.__context = ctypes.pointer(nfc.nfc_context())
+        nfc.nfc_init(ctypes.byref(self.__context))
+        conn_strings = (nfc.nfc_connstring * 10)()
+        devices_found = nfc.nfc_list_devices(self.__context, conn_strings, 10)
+        if devices_found >= 1:
+            self.__device = nfc.nfc_open(self.__context, conn_strings[0])
+            _ = nfc.nfc_initiator_init(self.__device)
+
+    def poll(self):
+        nt = nfc.nfc_target()
+        res = nfc.nfc_initiator_poll_target(self.__device, self.__modulations, len(self.__modulations), 1, 1, ctypes.byref(nt))
+        if res < 0:
+            return None
+        elif res == 0:
+            return None
+        uid = None
+        if nt.nti.nai.szUidLen == 4:
+            uid = "".join([format(nt.nti.nai.abtUid[i],'02x') for i in range(4)])
+        return uid
+
+    def __del__(self):
+        nfc.nfc_close(self.__device)
+        nfc.nfc_exit(self.__context)
+
 
 leds = Adafruit_NeoPixel(2, 13, channel=1)
 mx = MAX30102.MAX30102()
@@ -37,6 +85,9 @@ options.cols = 64
 options.disable_hardware_pulsing = True
 options.pixel_mapper_config = "Rotate:180"
 
+nfcRdr = NFCReader()
+nfcRdr.run()
+
 matrix = RGBMatrix(options = options)
 
 pixs = [255,255,255,255,255,0]
@@ -47,6 +98,19 @@ try:
     tgt = 0.4
     spd = 0.1
     while True:
+        uid = nfcRdr.poll()
+        if uid:
+            print "Got NFC card "+uid
+            if uid == 'e4c3b6c3' and heartbeatdelay != 120:
+                print "Stop beat"
+                heartbeatdelay = 120
+                spd = 0.1
+                tgt = 0.4
+            if uid == '9349a524' and heartbeatdelay == 120:
+                print "Start beat"
+                heartbeatdelay = 0
+                spd = 0.1
+                tgt = 0.4
         for x in range(0, matrix.width):
             for y in range(0, matrix.height):
                 matrix.SetPixel(x, y, 0, 0, 0)
@@ -101,7 +165,7 @@ try:
                 for y in range(ypos1, ypos2):
                     matrix.SetPixel(x, y, 0, 255, 0)
             matrix.SetPixel(x, ypos1, 0, 200, 0)
-            if heartbeatdelay >= -100:
+            if heartbeatdelay >= -100 and heartbeatdelay <= 100:
                 heartbeatdelay += 1
 
             try:
